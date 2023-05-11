e3eb23a1b4e24131f683c5ec0c9a54e02d3edd52
kv4rtz <119259462+kv4rtz@users.noreply.github.com>
2023-05-11 15:15:16 +0400

    Update Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -11,8 +11,8 @@ const Main = () => {
                     <h1 className={styles.title}>WELCOME TO THE NEW ERA OF <span>AFTER 2033</span></h1>
                     <p className={styles.text}>Reflection of the situation in the world. An exciting plot and a beautiful surrounding world!</p>
                     <div className={styles.buttons}>
-                        <Link className={styles.order} href='#'>PRE-ORDER</Link>
-                        <Link className={styles.tech} href='#'>TECHNICAL SUPPORT</Link>
+                        <Link className={styles.order} href='https://store.steampowered.com/?l=russian'>PRE-ORDER</Link>
+                        <Link className={styles.tech} href='/techsupport'>TECHNICAL SUPPORT</Link>
                     </div>
                     <i className={`fi fi-ss-angle-double-small-down ${styles.down}`}></i>
                 </div>
