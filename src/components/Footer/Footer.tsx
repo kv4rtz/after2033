008f1ff1229a53155838e04c656fd44ba76cda4d
kv4rtz <119259462+kv4rtz@users.noreply.github.com>
2023-05-12 16:29:24 +0400

    Add files via upload

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import styles from './Footer.module.css'
 import Image from "next/image"
 import Container from "@/components/Container/Container"
+import Link from "next/link";
 
 const Footer = () => {
     return (
@@ -12,6 +13,10 @@ const Footer = () => {
                     <a href="https://pegi.info" target={'_blank'}><Image width={166} height={166} src={'/violence.svg'} alt={'16+'}/></a>
                     <a href="https://pegi.info" target={'_blank'}><Image  width={166} height={166} src={'/fear.svg'} alt={'16+'}/></a>
                     <a href="https://pegi.info" target={'_blank'}><Image  width={166} height={166} src={'/language.svg'} alt={'16+'}/></a>
+                </div>
+                <div className={styles.links}>
+                    <Link href='/techsupport'>TECH SUPPORT</Link>
+                    <Link href="/aboutus">ABOUT US</Link>
                 </div>
             </Container>
         </footer>
